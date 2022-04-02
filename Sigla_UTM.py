71f4e1a911e1ae98d218bd85cd091745a765db06
dorintriboi <dorin.triboi@isa.utm.md>
2022-04-02 12:37:34 +0300

    Sigla UTM a fost modificata

--- Sigla_UTM.py
+++ Sigla_UTM.py
@@ -5,3 +5,6 @@ panza.pack()
 cerc1 = panza.create_oval(50,50,350,400, fill = "blue", activeoutline="green",width = 1)
 cerc2 = panza.create_oval(15,50,400,375, fill = "white", activeoutline="red", width = 1)
 pagina.mainloop()
+
+
+#primul coomit
